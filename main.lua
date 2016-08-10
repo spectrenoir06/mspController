d74826e0be45a540e773aca2de66116ab086b1c1
Spectre <antoine@doussaud.org>
2016-08-10 17:58:11 +0200

    serial

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ local band, bor, bxor = bit.band, bit.bor, bit.bxor
 local lshift, rshift, rol = bit.lshift, bit.rshift, bit.rol
 
 MSP_SET_RAW_RC = 200
+MSP_SERIAL_UUID = 232
 MSP_ALTITUDE   = 109
 MSP_IDENT      = 100
 MSP_BAT        = 110
@@ -81,14 +82,24 @@ function printInput(serial)
 end
 
 function love.load(arg)
-	--serial = Serial("/dev/ttyACM0", 9600)
+	serial = Serial("/dev/ttyUSB0", 115200)
 	roll, pitch, yaw, gaz = 0, 0, 0, 0
 	update_timer = 0
+	msg_disp = ""
+	love.graphics.setFont(love.graphics.newFont(32))
 end
 
 function msp_send(serial, type, size, data)
-	local msg = "$M<" .. struct.pack("bb",type, size)..data..struct.pack("b", crc32(data)).."\n"
-	io.write(msg)
+	if data == nil then data = "" end
+	-- checksum = bit.bxor(type,size)
+	-- for i = 1, #data do
+	-- 	local c = data:sub(i,i)
+	-- 	checksum = bit.bxor(checksum,c)
+	-- end
+	-- local msg = "$M<" .. struct.pack("bb",type, size) .. data .. struct.pack("b", checksum) --.. "\n"
+
+	local msg = "$M<" .. struct.pack("bbb", 0, 232, 232) --.. "\n"
+	serial:write(msg)
 end
 
 
@@ -114,15 +125,30 @@ function love.update(dt)
 
 	update_timer = update_timer + dt
 	if update_timer > 0.05 then
-		msp_send(io, MSP_SET_RAW_RC, 8, struct.pack("hhhhhhhh",1,2,3,4,5,6,7,8))
+		-- msp_send(serial, MSP_SET_RAW_RC, 8, struct.pack("hhhhhhhh",1,2,3,4,5,6,7,8))
 		update_timer = 0
+		msp_send(serial, MSP_SERIAL_UUID, 0, "")
+		data, buffer = readInput(serial)
+		if data then
+			local msg = ""
+			for i = 6, #buffer -1 do
+				local c = buffer:sub(i,i)
+				msg = msg..string.format("%x ",c:byte())
+			end
+			print(msg)
+			msg_disp = msg
+		end
 	end
+end
+
+function love.draw()
+	love.graphics.print("serial: "..msg_disp, 10, 10)
 end
 
 function love.keypressed(key, scancode, isrepeat)
 
 	if key == "space" then
-
+		love.system.setClipboardText(msg_disp)
 	end
 end
 
